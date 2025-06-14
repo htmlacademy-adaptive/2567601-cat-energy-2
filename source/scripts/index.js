e8d9ec02ae8f0108712d8ee9457693c848de78b1
noname <luizaxakieva@gmail.com>
2025-06-14 18:06:49 +0300

    The vector graphics (#6)
    * Adds svg for index.html
    
    * Adds vector graphics
    
    * Starts adding adaptive grids
    
    * Edit nav
    
    * Add adaptive grids

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+const button = document.querySelector('.main-header__toggle');
+const mainNav = document.querySelector('.main-nav');
+const mainHeaderNav = document.querySelector('.main-header__nav');
+
+button.onclick = function(){
+  if (!button.classList.contains('main-header__toggle--closed')){
+    button.classList.add('main-header__toggle--closed');
+    mainNav.classList.add('main-nav--opened');
+    mainHeaderNav.style.position = 'absolute';
+    mainHeaderNav.style.width = '100%';
+  } else {
+    button.classList.remove('main-header__toggle--closed');
+    mainNav.classList.remove('main-nav--opened');
+    mainHeaderNav.style.position = 'relative';
+  }
+};
