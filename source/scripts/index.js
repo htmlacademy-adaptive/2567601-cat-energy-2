777445c3fac308eb2f13560dc523e301b42e0697
noname <luizaxakieva@gmail.com>
2025-06-22 13:20:45 +0300

    Adds a fine grid
    * Adds a fine grid for the mobile version
    
    * Adds a fine grid for tablet
    
    * Adds a fine grid for desktop version(index)
    
    * Adds a fine grid for index version(catalog)
    
    * Adds a fine grid for tablet version(catalog)
    
    * Adds a fine grid for desktop version(catalog)
    
    * Adds a map
    
    * Edits the arrow
    
    * Corrects two pages

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,16 +1,15 @@
 const button = document.querySelector('.main-header__toggle');
-const mainNav = document.querySelector('.main-nav');
-const mainHeaderNav = document.querySelector('.main-header__nav');
+const mainNav = document.querySelector('.main-header__nav');
+
+mainNav.classList.remove('main-header__nav--nojs');
+button.classList.remove('main-header__toggle--nojs');
 
 button.onclick = function(){
   if (!button.classList.contains('main-header__toggle--closed')){
     button.classList.add('main-header__toggle--closed');
-    mainNav.classList.add('main-nav--opened');
-    mainHeaderNav.style.position = 'absolute';
-    mainHeaderNav.style.width = '100%';
+    mainNav.classList.add('main-header__nav--opened');
   } else {
     button.classList.remove('main-header__toggle--closed');
-    mainNav.classList.remove('main-nav--opened');
-    mainHeaderNav.style.position = 'relative';
+    mainNav.classList.remove('main-header__nav--opened');
   }
 };
